f34a1c1e746eb97d8668d1237d3aaf7ef0b0ed2f
javi3y <javieyprograms@gmail.com>
2024-05-19 02:38:06 +0330

    added frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+import PrimeVue from 'primevue/config';
+import Lara from '@/presets/lara';      //import preset
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(PrimeVue, {
+    unstyled: true,
+	pt: Lara
+});
+
+
+app.use(router)
+
+app.mount('#app')
